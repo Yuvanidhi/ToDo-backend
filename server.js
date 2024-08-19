e13eceaa05fa38cbbb0c022f7b3e048c030692d9
Yuvanidhi S <yuvanidhis@gmail.com>
2024-08-19 14:04:44 +0530

    vercel

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+require('dotenv').config();
 
 // Import routes
 const taskRoutes = require('./routes/taskRoutes');
@@ -9,8 +10,9 @@ const taskRoutes = require('./routes/taskRoutes');
 app.use(express.json());
 app.use(cors());
 
+const dbURI = process.env.MONGODB_URI;
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/todolist', {
+mongoose.connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 });
